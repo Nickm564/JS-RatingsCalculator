2101d848e6d76ee49f8635d7c1ead15b5e933da4
Nicholas Molina <nicholasmolina564@gmail.com>
2019-11-06 11:48:49 -0600

    Completed Module 3.5

--- js/ratings.js
+++ js/ratings.js
@@ -6,14 +6,14 @@ const collect_ratings = ( ) => {
   };
   let rating = 0;
 
-  const elements = document.querySelectorAll('.rating');
-  elements.forEach(element => {
-    rating = parseInt(element.id.replace( 'star', ' '));
+  const elements =  document.querySelectorAll('.rating');
+  elements.forEach(element  =>  {
+    rating = parseInt(element.id.replace('star', ' '));
     ratings.count += parseInt(element.value);
     ratings.sum += parseInt(element.value) * rating;
   });
 
-  if (rating.count !== 0) {
+  if  (ratings.count !== 0) {
     ratings.average = ratings.sum / ratings.count;
    }
    return ratings;
