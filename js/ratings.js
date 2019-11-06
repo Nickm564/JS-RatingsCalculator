2cfe60e960d21e0c472ae564dd6445fff312d4ad
Nicholas Molina <nicholasmolina564@gmail.com>
2019-11-06 11:59:00 -0600

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -6,7 +6,7 @@ const collect_ratings = ( ) => {
   };
   let rating = 0;
 
-  const elements =  document.querySelectorAll('.rating');
+  const elements  =  document.querySelectorAll('.rating');
   elements.forEach(element  =>  {
     rating = parseInt(element.id.replace('star', ' '));
     ratings.count += parseInt(element.value);
